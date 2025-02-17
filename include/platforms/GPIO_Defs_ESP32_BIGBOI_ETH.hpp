0051f2ce46d1b6fa8f7c7016b74f372b26aa0d16
Jason Theriault <jay.m.theriault@gmail.com>
2025-02-16 19:29:15 -0500

    Walk Back

--- include/platforms/GPIO_Defs_ESP32_BIGBOI_ETH.hpp
+++ include/platforms/GPIO_Defs_ESP32_BIGBOI_ETH.hpp
@@ -22,10 +22,9 @@
 // Output Manager
 #define DEFAULT_RMT_0_GPIO      gpio_num_t::GPIO_NUM_27
 #define DEFAULT_RMT_1_GPIO      gpio_num_t::GPIO_NUM_26
-#define DEFAULT_RMT_2_GPIO      gpio_num_t::GPIO_NUM_25
 #define DEFAULT_RMT_3_GPIO      gpio_num_t::GPIO_NUM_33
 #define DEFAULT_RMT_4_GPIO      gpio_num_t::GPIO_NUM_32
-#define DEFAULT_RMT_5_GPIO      gpio_num_t::GPIO_NUM_4
+
 
 
 // File Manager
@@ -58,24 +57,6 @@
 #define W5500_CS_PIN            gpio_num_t::GPIO_NUM_15  // Use GPIO 12 for W5500 CS
 #define W5500_RST_PIN           gpio_num_t::GPIO_NUM_2  // Use GPIO 16 for W5500 RST
 #define W5500_INT_PIN           gpio_num_t::GPIO_NUM_4  // Use GPIO 17 for W5500 INT (optional)
-
-// SPI Pins for W5500 (shared with SD card)
 #define W5500_MISO_PIN          gpio_num_t::GPIO_NUM_12
 #define W5500_MOSI_PIN          gpio_num_t::GPIO_NUM_13
 #define W5500_SCK_PIN           gpio_num_t::GPIO_NUM_14
-// Built-in Ethernet Configuration (optional, comment out if not used)
-/*
-#define DEFAULT_ETH_CLK_MODE           eth_clock_mode_t::ETH_CLOCK_GPIO0_IN
-#define DEFAULT_ETH_POWER_PIN          gpio_num_t::GPIO_NUM_15
-#define DEFAULT_ETH_POWER_PIN_ACTIVE   HIGH
-#define DEFAULT_ETH_TYPE               eth_phy_type_t::ETH_PHY_LAN8720
-#define DEFAULT_ETH_ADDR               ETH_ADDR_PHY_LAN8720
-#define DEFAULT_ETH_TXEN               gpio_num_t::GPIO_NUM_21
-#define DEFAULT_ETH_TXD0               gpio_num_t::GPIO_NUM_19
-#define DEFAULT_ETH_TXD1               gpio_num_t::GPIO_NUM_22
-#define DEFAULT_ETH_CRSDV              gpio_num_t::GPIO_NUM_27
-#define DEFAULT_ETH_RXD0               gpio_num_t::GPIO_NUM_25
-#define DEFAULT_ETH_RXD1               gpio_num_t::GPIO_NUM_26
-#define DEFAULT_ETH_MDC_PIN            gpio_num_t::GPIO_NUM_23
-#define DEFAULT_ETH_MDIO_PIN           gpio_num_t::GPIO_NUM_18
-*/
