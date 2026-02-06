fa90332170400686c6ddfc9e901c7391d06289fe
Martin Mueller <MartinMueller2003@yahoo.com>
2026-02-06 14:24:24 -0500

    Added redirex support for factory reset and reboot.

--- html/NodeJsServer.cjs
+++ html/NodeJsServer.cjs
@@ -12,8 +12,11 @@ app.get("/", (req, res) => {
     res.sendFile(__dirname + "/index.html");
 });
 
-
+app.get("/XP",           (req, res) => {res.redirect(espsIp + "XP")});
 app.post("/XP",          (req, res) => {res.redirect(espsIp + "XP")});
+app.post("/X6",          (req, res) => {res.redirect(espsIp + "X6")});
+app.post("/X7",          (req, res) => {res.redirect(espsIp + "X7")});
+app.post("/XJ",          (req, res) => {res.redirect(espsIp + "XJ")});
 app.get("/XJ",           (req, res) => {res.redirect(espsIp + "XJ")});
 app.put("/conf/:file",   (req, res) => {res.redirect(espsIp + "conf/" + req.params.file);});
 app.get("/conf/:file",   (req, res) => {res.redirect(espsIp + "conf/" + req.params.file);});
